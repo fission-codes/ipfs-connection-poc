6188e692c6440a11af6e5f8966f2b501f72d308d
Brian Ginsburg <gins@brianginsburg.com>
2021-10-21 11:25:16 -0700

    Add initial reporting implementation
    Add connection status to track online/offline, last connected time, and
    latency. We will want to report these on a window post when we bring
    this into the service worker.

--- main.js
+++ main.js
@@ -61,8 +61,33 @@ const main = async () => {
   const ipfs = await create(OPTIONS)
   self.ipfs = ipfs
 
+  // Track the connections in a closure
+  let connections = []
+
+  const report = (peer, status) => {
+    connections = connections
+      .filter(connection => connection.peer !== peer)
+      .concat({ peer, ...status })
+
+    const offline = connections.every(connection => !connection.connected)
+    const lastConnectedAt = connections.reduce((newest, connection) =>
+      newest >= connection.lastConnectedAt ? newest : connection.lastConnectedAt,
+      0
+    )
+
+    const activeConnections = connections.filter(connection => connection.latency !== null)
+    const averageLatency = activeConnections.length > 0
+      ? connections.reduce((sum, connection) => sum + connection.latency, 0) / activeConnections.length
+      : null
+
+    console.table(connections)
+    log('offline', offline)
+    log('last connected at', lastConnectedAt === 0 ? null : lastConnectedAt)
+    log('average latency', averageLatency)
+  }
+
   peers.forEach(peer => {
-    tryConnecting(peer)
+    tryConnecting(peer, report)
   })
 }
 
@@ -83,7 +108,7 @@ function fetchPeers() {
 
 let latestTimeoutId = null;
 
-async function keepAlive(peer, backoff) {
+async function keepAlive(peer, backoff, status, report) {
   log('retry number', backoff.retryNumber)
 
   let timeoutId = null;
@@ -92,16 +117,22 @@ async function keepAlive(peer, backoff) {
     log('backoff timeout', backoff.currentBackoff)
 
     // Start race between reconnect and ping
-    timeoutId = setTimeout(() => reconnect(peer, backoff), backoff.currentBackoff)
+    timeoutId = setTimeout(() => reconnect(peer, backoff, status, report), backoff.currentBackoff)
 
     // Track the latest reconnect attempt
     latestTimeoutId = timeoutId;
   } else {
     console.log('max connection attempts exceeded, giving up')
+
+    const updatedStatus = { ...status, connected: false, latency: null }
+    report(peer, updatedStatus)
   }
 
-  self.ipfs.libp2p.ping(peer).then(() => {
+  self.ipfs.libp2p.ping(peer).then(latency => {
     log('alive')
+
+    const updatedStatus = { connected: true, lastConnectedAt: Date.now(), latency }
+    report(peer, updatedStatus)
 
     // Cancel reconnect because ping won
     clearTimeout(timeoutId)
@@ -109,14 +140,17 @@ async function keepAlive(peer, backoff) {
     // Keep alive after the latest ping-reconnect race, ignore the rest
     // Give up if timeoutId is null, we are at max retries
     if (timeoutId === latestTimeoutId) {
-      setTimeout(() => keepAlive(peer, BACKOFF_INIT), KEEP_ALIVE_INTERVAL)
+      setTimeout(() => keepAlive(peer, BACKOFF_INIT, updatedStatus, report), KEEP_ALIVE_INTERVAL)
     }
   }).catch(() => { })
 
 }
 
-async function reconnect(peer, { retryNumber, lastBackoff, currentBackoff }) {
+async function reconnect(peer, { retryNumber, lastBackoff, currentBackoff }, status, report) {
   log('reconnecting')
+
+  const updatedStatus = { ...status, connected: false, latency: null }
+  report(peer, updatedStatus)
 
   try {
     await self.ipfs.swarm.disconnect(peer)
@@ -125,30 +159,43 @@ async function reconnect(peer, { retryNumber, lastBackoff, currentBackoff }) {
     // No action needed, we will retry
   }
 
+  const backoff = {
+    retryNumber: retryNumber + 1,
+    lastBackoff: currentBackoff,
+    currentBackoff: lastBackoff + currentBackoff
+  }
+
   // Keep alive or keep trying after this reconnect attempt
-  keepAlive(peer, { retryNumber: retryNumber + 1, lastBackoff: currentBackoff, currentBackoff: lastBackoff + currentBackoff })
+  keepAlive(peer, backoff, updatedStatus, report)
 }
 
-async function tryConnecting(peer) {
+async function tryConnecting(peer, report) {
   self
     .ipfs.libp2p.ping(peer)
-    .then(() => {
+    .then(latency => {
+
       return ipfs.swarm
         .connect(peer, 1 * 1000)
         .then(() => {
           console.log(`🪐 Connected to ${peer}`)
 
+          const status = { connected: true, lastConnectedAt: Date.now(), latency }
+          report(peer, status)
+
           // Ensure permanent connection to Fission gateway
           // TODO: This is a temporary solution while we wait for
           //       https://github.com/libp2p/js-libp2p/issues/744
           //       (see "Keep alive" bit)
-          setTimeout(() => keepAlive(peer, BACKOFF_INIT), KEEP_ALIVE_INTERVAL)
+          setTimeout(() => keepAlive(peer, BACKOFF_INIT, status, report), KEEP_ALIVE_INTERVAL)
         })
     })
     .catch(() => {
       console.log(`🪓 Could not connect to ${peer}. Will keep trying.`)
 
-      keepAlive(peer, BACKOFF_INIT)
+      const status = { connected: false, lastConnectedAt: 0, latency: null }
+      report(peer, status)
+
+      keepAlive(peer, BACKOFF_INIT, status, report)
     })
 }
 
